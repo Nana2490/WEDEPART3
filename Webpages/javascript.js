b88f8ae6f41f3d72d71ce0b79ec2fcde9c16f6b9
Nana2490 <st10479313@rcconnect.edu.za>
2025-05-30 18:14:25 +0000

    Added the auto year change to footer
    i added the js to all html files and then also made the footer be able to change years automatically.

---
+++ Webpages/javascript.js
@@ -0,0 +1,7 @@
+var year = new Date().getFullYear();
+
+var date = ` Privacy Statement: SheFit Fitness Gym is committed to protecting your personal information. We do not share or sell your details to any third parties.
+<br> &copy; Mphatso Phiri Codes ${year}. All Rights Reserved.`;
+
+document.getElementsByTagName('footer')[0].innerHTML = date;
+
