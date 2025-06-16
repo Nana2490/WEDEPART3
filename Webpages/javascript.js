027da5b7ff7e5db0094a5c480207108d1762c404
Nana2490 <st10479313@rcconnect.edu.za>
2025-06-16 07:14:43 +0000

    Updated js. to website for footer and email function
    had to update js. function

--- Webpages/javascript.js
+++ Webpages/javascript.js
@@ -5,3 +5,18 @@ var date = ` Privacy Statement: SheFit Fitness Gym is committed to protecting yo
 
 document.getElementsByTagName('footer')[0].innerHTML = date;
 
+const form = document.querySelector('form');
+
+function sendEmail() {
+    Email.send({
+        Host : "s1.maildns.net",
+        Username : "username",
+        Password : "password",
+        To : 'them@website.com',
+        From : "you@isp.com",
+        Subject : "This is the subject",
+        Body : "And this is the body"
+    }).then(
+      message => alert(message)
+    );
+}
